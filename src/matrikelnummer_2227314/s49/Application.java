7162707d9e58adf5579b92958a813ea35d3c266d
Johannes | A <Jeis2519@gmail.com>
2020-03-06 12:39:13 +0100

    added comment for debug message

--- src/matrikelnummer_2227314/s49/Application.java
+++ src/matrikelnummer_2227314/s49/Application.java
@@ -3,7 +3,7 @@ package matrikelnummer_2227314.s49;
 import matrikelnummer_2227314.s49.airplane.Airplane;
 
 public class Application {
-    public static final boolean debugMessages = false;
+    public static final boolean debugMessages = false; // für mehr und genaueren Output auf true setzen
     public static void main(String... args) {
         Airplane airplane = new Airplane("Airbus A380");
         airplane.start();
